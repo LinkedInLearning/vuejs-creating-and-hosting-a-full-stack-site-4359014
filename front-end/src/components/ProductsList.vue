689e988205905c91d2e808d64630e6cb24bd9f06
Shaun Wassell <shaun.p.wassell@gmail.com>
2023-01-17 22:36:25 +0000

    Moved products list to a reusable component

---
+++ front-end/src/components/ProductsList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="grid-wrap">
+    <div class="product-item" v-for="product in products" :key="product.id">
+      <img :src="product.imageName" />
+      <h3 class="product-name">{{ product.name }}</h3>
+      <p class="product-price">{{ product.price }}</p>
+      <router-link :to="'/products/' + product.id">
+        <button>View Details</button>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductsList",
+  props: ['products'],
+}
+</script>
--- front-end/src/pages/ProductsPage.vue
+++ front-end/src/pages/ProductsPage.vue
@@ -1,26 +1,17 @@
 <template>
   <h1>Products</h1>
-  <div class="grid-wrap">
-    <div
-      class="product-item"
-      v-for="product in products"
-      :key="product.id"
-    >
-      <img :src="product.imageName" />
-      <h3 class="product-name">{{ product.name }}</h3>
-      <p class="product-price">{{ product.price }}</p>
-      <router-link :to="'/products/' + product.id">
-        <button>View Details</button>
-      </router-link>
-    </div>
-  </div>
+  <ProductsList :products="products"/>
 </template>
 
 <script>
+import ProductsList from '../components/ProductsList.vue';
 import { products } from '../temp-data';
 
 export default {
   name: "ProductsPage",
+  components: {
+    ProductsList,
+  },
   data() {
     return {
       products,
