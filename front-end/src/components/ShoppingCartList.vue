24039330a0cfa30f65d104a4f3dbef004ab88d24
Shaun Wassell <shaun.p.wassell@gmail.com>
2023-01-17 23:00:20 +0000

    Created a reusable Shopping Cart List component

---
+++ front-end/src/components/ShoppingCartList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="product-container" v-for="product in products" :key="product.id">
+    <img class="product-image" :src="product.imageName" />
+    <div class="details-wrap">
+      <h3>{{ product.name }}</h3>
+      <p>{{ product.price }}</p>
+    </div>
+    <button class="remove-button">Remove from Cart</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ShoppingCartList",
+  props: ['products']
+}
+</script>
