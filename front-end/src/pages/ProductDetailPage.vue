654e6cd2fa3a937ca82f214e069bb6d497cef74d
Shaun Wassell <shaun.p.wassell@gmail.com>
2023-01-17 20:47:08 +0000

    Implemented the Product Detail Page

--- front-end/src/pages/ProductDetailPage.vue
+++ front-end/src/pages/ProductDetailPage.vue
@@ -1,9 +1,23 @@
 <template>
-  <h1>Product Detail Page</h1>
+  <div class="img-wrap">
+    <img :src="product.imageName" />
+  </div>
+  <div class="product-details">
+    <h1>{{ product.name }}</h1>
+    <h3 class="price">{{ product.price }}</h3>
+    <button class="add-to-cart">Add to cart</button>
+  </div>
 </template>
 
 <script>
+import { products } from '../temp-data';
+
 export default {
   name: "ProductDetailPage",
+  data() {
+    return {
+      product: products.find(product => product.id === this.$route.params.productId),
+    }
+  }
 }
 </script>
