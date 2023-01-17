22e0089fd00cf72725d2ccde25e67a09f5a12677
Shaun Wassell <shaun.p.wassell@gmail.com>
2023-01-17 21:47:27 +0000

    Linked the products to the corresponding detail pages

--- front-end/src/pages/ProductsPage.vue
+++ front-end/src/pages/ProductsPage.vue
@@ -9,7 +9,9 @@
       <img :src="product.imageName" />
       <h3 class="product-name">{{ product.name }}</h3>
       <p class="product-price">{{ product.price }}</p>
-      <button>View Details</button>
+      <router-link :to="'/products/' + product.id">
+        <button>View Details</button>
+      </router-link>
     </div>
   </div>
 </template>
