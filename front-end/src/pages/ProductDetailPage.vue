bbd0afedf7fb680bdec46427ff8c76be63009192
Shaun Wassell <shaun.p.wassell@gmail.com>
2023-01-18 23:33:18 +0000

    Shows if an item is already in the user's cart

--- front-end/src/pages/ProductDetailPage.vue
+++ front-end/src/pages/ProductDetailPage.vue
@@ -6,7 +6,8 @@
       <div class="product-details">
         <h1>{{ product.name }}</h1>
         <h3 class="price">{{ product.price }}</h3>
-        <button @click="addToCart" class="add-to-cart">Add to cart</button>
+        <button @click="addToCart" class="add-to-cart" v-if="!itemIsInCart">Add to cart</button>
+        <button class="grey-button" v-if="itemIsInCart">Item is already in cart</button>
       </div>
   </div>
   <div v-else>
@@ -23,6 +24,12 @@ export default {
   data() {
     return {
       product: {},
+      cartItems: [],
+    }
+  },
+  computed: {
+    itemIsInCart() {
+      return this.cartItems.some(item => item.id === this.$route.params.productId);
     }
   },
   methods: {
@@ -38,6 +45,10 @@ export default {
     const response = await axios.get(`/api/products/${this.$route.params.productId}`);
     const product = response.data;
     this.product = product;
+
+    const cartResponse = await axios.get('/api/users/12345/cart');
+    const cartItems = cartResponse.data;
+    this.cartItems = cartItems;
   }
 }
 </script>
