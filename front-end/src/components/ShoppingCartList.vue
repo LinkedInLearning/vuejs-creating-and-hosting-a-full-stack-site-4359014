7e06064ca65a2f70b5482d43c300ce0bdf9f847f
Shaun Wassell <shaun.p.wassell@gmail.com>
2023-01-18 23:12:47 +0000

    Make request to back-end when removing item from cart

--- front-end/src/components/ShoppingCartList.vue
+++ front-end/src/components/ShoppingCartList.vue
@@ -5,7 +5,7 @@
       <h3>{{ product.name }}</h3>
       <p>{{ product.price }}</p>
     </div>
-    <button class="remove-button">Remove from Cart</button>
+    <button @click="$emit('remove-from-cart', product.id)" class="remove-button">Remove from Cart</button>
   </div>
 </template>
 
--- front-end/src/pages/ShoppingCartPage.vue
+++ front-end/src/pages/ShoppingCartPage.vue
@@ -1,7 +1,7 @@
 <template>
   <h1>Shopping Cart</h1>
   <div v-if="cartItems.length > 0">
-    <ShoppingCartList :products="cartItems" />
+    <ShoppingCartList @remove-from-cart="removeFromCart($event)" :products="cartItems" />
     <button class="checkout-button">Proceed to Checkout</button>
   </div>
   <div v-if="cartItems.length === 0">
@@ -22,6 +22,13 @@ export default {
     return {
       cartItems: [],
     }
+  },
+  methods: {
+    async removeFromCart(productId) {
+      const response = await axios.delete(`/api/users/12345/cart/${productId}`);
+      const updatedCart = response.data;
+      this.cartItems = updatedCart;
+    },
   },
   async created() {
     const response = await axios.get('/api/users/12345/cart');
