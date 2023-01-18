79a79a930b26784f7b94c2d6bdfaa2c10c60aa7f
Shaun Wassell <shaun.p.wassell@gmail.com>
2023-01-18 18:17:25 +0000

    Added an endpoint for adding items to cart

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,7 +1,11 @@
 import express from 'express';
-import { cartItems, products } from './temp-data';
+import { cartItems as cartItemsRaw, products as productsRaw } from './temp-data';
+
+let cartItems = cartItemsRaw;
+let products = productsRaw;
 
 const app = express();
+app.use(express.json());
 
 app.get('/hello', (req, res) => {
   res.send('Hello!');
@@ -19,6 +23,13 @@ app.get('/products/:productId', (req, res) => {
   const productId = req.params.productId;
   const product = products.find(product => product.id === productId);
   res.json(product);
+});
+
+app.post('/cart', (req, res) => {
+  const productId = req.body.id;
+  const product = products.find(product => product.id === productId);
+  cartItems.push(product);
+  res.json(cartItems);
 });
 
 app.listen(8000, () => {
