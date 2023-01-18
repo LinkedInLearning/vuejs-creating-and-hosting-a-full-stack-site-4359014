aaae1b508a74d0e3c52794940e64dc946cc8c71d
Shaun Wassell <shaun.p.wassell@gmail.com>
2023-01-18 22:15:14 +0000

    Loaded products from the back-end on the Products Page

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -11,7 +11,7 @@ async function start() {
   const app = express();
   app.use(express.json());
 
-  app.get('/products', async (req, res) => {
+  app.get('/api/products', async (req, res) => {
     const products = await db.collection('products').find({}).toArray();
     res.send(products);
   });
@@ -20,19 +20,19 @@ async function start() {
     return Promise.all(ids.map(id => db.collection('products').findOne({ id })));
   }
 
-  app.get('/users/:userId/cart', async (req, res) => {
+  app.get('/api/users/:userId/cart', async (req, res) => {
     const user = await db.collection('users').findOne({ id: req.params.userId });
     const populatedCart = await populateCartIds(user.cartItems);
     res.json(populatedCart);
   });
 
-  app.get('/products/:productId', async (req, res) => {
+  app.get('/api/products/:productId', async (req, res) => {
     const productId = req.params.productId;
     const product = await db.collection('products').findOne({ id: productId });
     res.json(product);
   });
 
-  app.post('/users/:userId/cart', async (req, res) => {
+  app.post('/api/users/:userId/cart', async (req, res) => {
     const userId = req.params.userId;
     const productId = req.body.id;
 
@@ -45,7 +45,7 @@ async function start() {
     res.json(populatedCart);
   });
 
-  app.delete('/users/:userId/cart/:productId', async (req, res) => {
+  app.delete('/api/users/:userId/cart/:productId', async (req, res) => {
     const userId = req.params.userId;
     const productId = req.params.productId;
 
--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -2,4 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    proxy: 'http://localhost:8000'
+  }
 })
