ee6a20ef74b493ff9785450cce6a651375837f09
Shaun Wassell <shaun.p.wassell@gmail.com>
2023-01-18 23:00:54 +0000

    Added items to shopping cart with axios

--- front-end/src/pages/ProductDetailPage.vue
+++ front-end/src/pages/ProductDetailPage.vue
@@ -6,7 +6,7 @@
       <div class="product-details">
         <h1>{{ product.name }}</h1>
         <h3 class="price">{{ product.price }}</h3>
-        <button class="add-to-cart">Add to cart</button>
+        <button @click="addToCart" class="add-to-cart">Add to cart</button>
       </div>
   </div>
   <div v-else>
@@ -23,6 +23,12 @@ export default {
   data() {
     return {
       product: {},
+    }
+  },
+  methods: {
+    async addToCart() {
+      await axios.post('/api/users/12345/cart', { id: this.$route.params.productId });
+      alert('Successfully added item to cart!');
     }
   },
   components: {
