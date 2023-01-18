07019699625e275de11ebb529042b28e19e9095f
Shaun Wassell <shaun.p.wassell@gmail.com>
2023-01-18 17:36:33 +0000

    Added 3 routes for loading data

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,9 +1,24 @@
 import express from 'express';
+import { cartItems, products } from './temp-data';
 
 const app = express();
 
 app.get('/hello', (req, res) => {
   res.send('Hello!');
+});
+
+app.get('/products', (req, res) => {
+  res.json(products);
+});
+
+app.get('/cart', (req, res) => {
+  res.json(cartItems);
+});
+
+app.get('/products/:productId', (req, res) => {
+  const productId = req.params.productId;
+  const product = products.find(product => product.id === productId);
+  res.json(product);
 });
 
 app.listen(8000, () => {
