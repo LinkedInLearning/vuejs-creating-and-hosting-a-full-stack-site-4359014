f91bde60b148847333cbb58343c289b7403a627a
Shaun Wassell <shaun.p.wassell@gmail.com>
2023-01-19 20:38:32 +0000

    Hides sign out button when user is not logged in

--- front-end/src/components/NavBar.vue
+++ front-end/src/components/NavBar.vue
@@ -5,20 +5,32 @@
         <img :src="logo" />
       </div>
     </router-link>
-    <router-link to="/cart" class="cart-link">
-      <button>Shopping Cart</button>
-    </router-link>
+    <div class="nav-buttons-wrap">
+      <button @click="signOut" v-if="user">Sign Out</button>
+      <router-link to="/cart">
+        <button>Shopping Cart</button>
+      </router-link>
+    </div>
+
   </div>
 </template>
 
 <script>
+import { getAuth, signOut } from 'firebase/auth';
 import logo from '@/assets/logo-hexagon.svg';
 
 export default {
   name: "NavBar",
+  props: ['user'],
   data() {
     return {
       logo,
+    }
+  },
+  methods: {
+    signOut() {
+      const auth = getAuth();
+      signOut(auth);
     }
   }
 }
