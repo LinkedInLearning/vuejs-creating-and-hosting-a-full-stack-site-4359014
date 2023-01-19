b7b4540c83b618a4de0ac72828895481fcdcd05d
Shaun Wassell <shaun.p.wassell@gmail.com>
2023-01-19 19:24:25 +0000

    Added Firebase setup code

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -7,6 +7,19 @@ import ProductsPage from './pages/ProductsPage.vue';
 import ProductDetailPage from './pages/ProductDetailPage.vue';
 import NotFoundPage from './pages/NotFoundPage.vue';
 
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBQ6ak2u7zhyRRBf2iNF9jGxsPQjKtkGg0",
+  authDomain: "vue-site-301f8.firebaseapp.com",
+  projectId: "vue-site-301f8",
+  storageBucket: "vue-site-301f8.appspot.com",
+  messagingSenderId: "127414253778",
+  appId: "1:127414253778:web:645d4ec8ef61a2e22b550b"
+};
+
+initializeApp(firebaseConfig);
+
 createApp(App)
 .use(VueRouter.createRouter({
   history: VueRouter.createWebHistory(process.env.BASE_URL),
