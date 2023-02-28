4d9a8f16102f3977309f330029a97756751cad6e
Shaun Wassell <shaun.p.wassell@gmail.com>
2023-02-28 02:51:25 +0000

    Added warning message to firebase config

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -10,12 +10,7 @@ import NotFoundPage from './pages/NotFoundPage.vue';
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBQ6ak2u7zhyRRBf2iNF9jGxsPQjKtkGg0",
-  authDomain: "vue-site-301f8.firebaseapp.com",
-  projectId: "vue-site-301f8",
-  storageBucket: "vue-site-301f8.appspot.com",
-  messagingSenderId: "127414253778",
-  appId: "1:127414253778:web:645d4ec8ef61a2e22b550b"
+  // COPY AND PASTE YOUR FIREBASE CONFIG INFO HERE
 };
 
 initializeApp(firebaseConfig);
