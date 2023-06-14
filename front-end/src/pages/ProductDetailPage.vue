69d3319cd40e28f73c0c1fa512b73705a26221b5
Jude Abormegah <jude.abormegah@gmail.com>
2023-06-14 18:41:32 +0000

    Add product detail

--- front-end/src/pages/ProductDetailPage.vue
+++ front-end/src/pages/ProductDetailPage.vue
@@ -1,9 +1,23 @@
 <template>
-  <h1>Product Detail page</h1>
+  <div class="image-wrap">
+    <img :src="product.imageName"/>
+  </div>
+  <div class="product-details">
+    <h1>{{ product.name }}</h1>
+    <h3 class="price">{{ product.price }}</h3>
+    <button class="add-to-cart">Add to cart</button>
+  </div>
 </template>
 
 <script>
+import { products } from '@/temp-data';
+
 export default{
   name: "ProductDetailPage",
+  data(){
+    return{
+      product: products.find(p => p.id === this.$route.params.productId),
+    }
+  }
 }
 </script>
