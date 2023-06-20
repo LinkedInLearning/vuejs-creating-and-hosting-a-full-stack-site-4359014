53dd86fdb1e38e38526447b0523019607f205aa2
Jude Abormegah <jude.abormegah@gmail.com>
2023-06-20 19:40:15 +0000

    Add nodemon

---
+++ back-end/src/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import { cartItems, products } from "./temp-data";
+
+const app = express();
+
+app.get("/hello", (req, res) => {
+  res.send("Hello!");
+});
+
+app.get("/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/cart", (req, res) => {
+  res.json(cartItems);
+});
+
+app.get("/product/:productId", (req, res) => {
+  const productId = req.params.productId;
+  const product = products.find((product) => product.id === productId);
+  res.json(product);
+});
+
+app.listen(8000, () => {
+  console.log("Server is listening on port 8000");
+});
