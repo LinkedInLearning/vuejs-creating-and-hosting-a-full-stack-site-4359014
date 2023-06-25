815b4268234470bb41d5d4eef25f157883c17bff
Jude Abormegah <jude.abormegah@gmail.com>
2023-06-25 21:43:52 +0000

    Add mongosh

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,7 +1,14 @@
 import express from "express";
-import { cartItems, products } from "./temp-data";
+import {
+  cartItems as cartItemsRaw,
+  products as productsRaw,
+} from "./temp-data";
+
+let cartItems = cartItemsRaw;
+let products = productsRaw;
 
 const app = express();
+app.use(express.json());
 
 app.get("/hello", (req, res) => {
   res.send("Hello!");
@@ -11,14 +18,33 @@ app.get("/products", (req, res) => {
   res.json(products);
 });
 
+function populateCartIds(ids) {
+  return ids.map((id) => products.find((product) => product.id === id));
+}
+
 app.get("/cart", (req, res) => {
-  res.json(cartItems);
+  const populatedCart = populateCartIds(cartItems);
+  res.json(populatedCart);
 });
 
 app.get("/product/:productId", (req, res) => {
   const productId = req.params.productId;
   const product = products.find((product) => product.id === productId);
   res.json(product);
+});
+
+app.post("/cart", (req, res) => {
+  const productId = req.body.id;
+  cartItems.push(productId);
+  const populatedCart = populateCartIds(cartItems);
+  res.json(populatedCart);
+});
+
+app.delete("/cart/:productId", (req, res) => {
+  const productId = req.params.productId;
+  cartItems = cartItems.filter((id) => id !== productId);
+  const populatedCart = populateCartIds(cartItems);
+  res.json(populatedCart);
 });
 
 app.listen(8000, () => {
--- back-end/src/temp-data.js
+++ back-end/src/temp-data.js
@@ -1,80 +1,52 @@
-// import basilBottle from "./assets/basil-bottle.webp";
-// import lemonBottle from "./assets/lemon-bottle.webp";
-// import garlicBottle from "./assets/garlic-bottle.webp";
-// import rosemaryBottle from "./assets/rosemary-bottle.webp";
-// import chiliBottle from "./assets/chili-bottle.webp";
-// import delicateBottle from "./assets/delicate-bottle.webp";
-// import mediumBottle from "./assets/medium-bottle.webp";
-// import boldBottle from "./assets/bold-bottle.webp";
-
-export const cartItems = [
-  {
-    id: "123",
-    name: "Basil",
-    price: "$26.00",
-    // imageName: basilBottle,
-  },
-  {
-    id: "234",
-    name: "Lemon",
-    price: "$26.00",
-    // imageName: lemonBottle,
-  },
-  {
-    id: "345",
-    name: "Garlic",
-    price: "$26.00",
-    // imageName: garlicBottle,
-  },
-];
+export const cartItems = ["123", "345"];
 
 export const products = [
   {
     id: "123",
     name: "Basil",
     price: "$26.00",
-    // imageName: basilBottle,
+    imageUrl: "/images/basil-bottle.webp",
   },
   {
     id: "234",
     name: "Lemon",
     price: "$26.00",
-    // imageName: lemonBottle,
+    imageUrl: "/images/lemon-bottle.webp",
   },
   {
     id: "345",
     name: "Garlic",
     price: "$26.00",
-    // imageName: garlicBottle,
+    imageUrl: "/images/garlic-bottle.webp",
   },
   {
     id: "456",
     name: "Rosemary",
     price: "$26.00",
-    // imageName: rosemaryBottle,
+    imageUrl: "/images/rosemary-bottle.webp",
   },
   {
     id: "567",
     name: "Chili",
     price: "$26.00",
-    // imageName: chiliBottle,
+    imageUrl: "/images/chili-bottle.webp",
   },
   {
     id: "678",
     name: "Delicate",
     price: "$24.50",
-    // imageName: delicateBottle,
+    imageUrl: "/images/delicate-bottle.webp",
   },
   {
     id: "789",
     name: "Medium",
     price: "$24.50",
-    // imageName: mediumBottle,
+    imageUrl: "/images/medium-bottle.webp",
   },
   {
     id: "890",
     name: "Bold",
     price: "$24.50",
-    // imageName: boldBottle,
+    imageUrl: "/images/bold-bottle.webp",
   },
 ];
