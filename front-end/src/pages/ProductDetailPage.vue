f211c76b922f08711bf658513aab0955b6657d5a
Jude Abormegah <jude.abormegah@gmail.com>
2023-07-10 21:15:55 +0000

    check if item is in cart

--- front-end/src/pages/ProductDetailPage.vue
+++ front-end/src/pages/ProductDetailPage.vue
@@ -6,7 +6,8 @@
     <div class="product-details">
       <h1>{{ product.name }}</h1>
       <h3 class="price">{{ product.price }}</h3>
-      <button @click="addToCart" class="add-to-cart">Add to cart</button>
+      <button @click="addToCart" class="add-to-cart" v-if="!itemIsInCart">Add to cart</button>
+      <button class="grey-button" v-if="itemIsInCart">Item is already in cart</button>
     </div>
   </div>
   <div v-else>
@@ -20,9 +21,15 @@ import NotFoundPage from '@/pages/NotFoundPage.vue';
 
 export default{
   name: "ProductDetailPage",
-  data(){
+  data() {
     return{
       product: {},
+      cartItems: [],
+    }
+  },
+  computed: {
+    itemIsInCart() {
+      return this.cartItems.some(items => items.id === this.$route.params.productId);
     }
   },
   methods: {
@@ -38,6 +45,10 @@ export default{
     const response = await axios.get(`/api/products/${this.$route.params.productId}`);
     const product = response.data;
     this.product = product;
+
+    const cartItemsResponse = await axios.get('/api/users/12345/cart');
+    const cartItems = cartItemsResponse.data;
+    this.cartItems = cartItems;
   }
 }
 </script>
