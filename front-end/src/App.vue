9eb90be5dd881248198867b21e7b8354e27f3e43
toomanyfishb <150994039+toomanyfishb@users.noreply.github.com>
2024-03-05 01:38:13 +0000

    adding vue files

---
+++ front-end/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Hello from Vue</h1>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
